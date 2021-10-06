ec01ddec66088c409b3106e24d28385f1f08f969
Furentsu <eugeniofranzoso@gmail.com>
2021-10-06 16:15:43 +0200

    Added top part of the first main's section "construction projects"

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>
-      
-  </div>
+   <main>
+
+      <!-- Section between the jumbotron and the construction services offered  -->
+      <section class="construction-projects-top">
+          <h2>Do You Have A Construction Project We Can Help With?</h2>
+          <button>Get a free quote</button>
+      </section>
+
+      <!-- Section containing the construction services  -->
+      <section class="construction-projects-bottom">
+          
+      </section>
+
+   </main>
 </template>
 
 <script>
@@ -13,5 +24,21 @@ export default {
 
 <style lang="scss" scoped>
 @import "../style/variables.scss";
+
+.construction-projects-top {
+border: 1px solid red;
+
+    display: flex;
+    position: relative;
+    justify-content: center;
+    align-items: center;
+    height: 130px;
+
+    button {
+        position: absolute;
+        bottom: -15px;
+    }
+
+}
 
 </style>
