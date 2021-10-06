9395ec6e9ff12171ee6661d66875182d1ed71aa7
Furentsu <eugeniofranzoso@gmail.com>
2021-10-06 12:21:35 +0200

    Initialization of the project

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+
+    <!-- Logo -->
+    <div class="logo_container">
+      <img src="../assets/images/construction_logo_retina.png" alt="Avada Construction's logo">
+    </div>
+
+    <!-- Navbar -->
+    <nav>
+
+    </nav>
+
+    <!-- Get quote button -->
+    <button>
+
+    </button>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+
+<style lang="scss" scoped>
+@import "../style/variables.scss";
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Main</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+}
+</script>
+
+
+<style lang="scss" scoped>
+@import "../style/variables.scss";
+
+</style>
