7daa7139519677d9829ce881071eec8cfefac8ee
Furentsu <eugeniofranzoso@gmail.com>
2021-10-07 15:45:24 +0200

    Footer's contact section's structure completed

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,8 @@
     <div class="row">
       <FooterAbout />
       <FooterTweets />
-
+      <FooterContacts :contacts="contacts"/>
+      
 
     </div>
   </footer>
@@ -12,6 +13,8 @@
 <script>
 import FooterAbout from './FooterAbout.vue'
 import FooterTweets from './FooterTweets.vue'
+import FooterContacts from './FooterContacts.vue'
+
 
 
 export default {
@@ -19,7 +22,42 @@ export default {
   components: {
     FooterAbout,
     FooterTweets,
+    FooterContacts,
     
+    },
+    data() {
+      return {
+        contacts: [
+          {
+            iconClass: 'fas fa-globe',
+            text: 'Corporate location',
+            address: '1600 Amphitheatre Parkway',
+            postCode: 'London Wc11BA'
+          },
+          {
+            iconClass: 'fas fa-home',
+            text: 'Residential Location',
+            address: '1600 Amphitheatre Parkway',
+            postCode: 'London Wc11BA'
+          },
+          {
+            iconClass: 'fas fa-phone',
+            text: '1.800.458.556 / 1.800.532.2112',
+          },
+          {
+            iconClass: 'fas fa-envelope',
+            text: 'info@your-domain.com',
+          },
+          {
+            iconClass: 'fas fa-clock',
+            text: 'Monday - Friday: 9:00 AM - 6:00 PM',
+          },
+          {
+            iconClass: 'fas fa-clock',
+            text: 'Saturday - Sunday: 9:00 AM - 12:00 PM',
+          },
+        ]
+      }
     }
 }
 </script>
---
+++ src/components/FooterContacts.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="col-3">
+      <h3>Contact Us Today</h3>
+
+      <ul>
+        <li v-for="(element,index) in contacts" :key="index">
+            <div class="icon-container">
+                <i :class="contacts[index].iconClass"></i>
+            </div>
+
+            <div class="content ">
+                <span class="d-block">{{contacts[index].text}}</span>
+                <span class="d-block">{{contacts[index].address}}</span>
+                <span class="d-block">{{contacts[index].postCode}}</span>
+            </div>
+        </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'FooterContacts',
+    props: ['contacts']
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../style/variables.scss";
+
+ul {
+    list-style: none;
+    padding-left: 0;
+
+    li {
+        display: flex;
+    }
+}
+
+
+</style>
